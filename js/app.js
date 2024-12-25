953455fee42656d376c1117b5de1e62d7b8677a0
sumayyafozilova <sfozilova571@gmail.com>
2024-12-25 22:57:15 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// const soat = 11
+// if (soat <= 7){
+//    console.log("Xayrli tong");
+   
+// } else if (soat <= 12) {
+//    console.log("Xayrli kun");
+   
+// }
+//  else if (soat => 18) {
+//     console.log("Xayrl kech");
+//     }
+// else if (soat => 21) {
+//         console.log("Xayrl tun");
+//         }
+// const ega = prompt("Yoshingizni kiriting")
+// if ( ega >=18){
+//     console.log('Guvohnoma berilsin');
+//     }else{
+//        console.log (`guvoxnoma berilmasin ${18-ega} yil  kuting` );
+       
+//     }
+// const asr = prompt('yilingizni kiriting')
+// if (asr <= 2000){
+//     console.log('21-asrda');
+//     } else  {
+//         console.log(`20-arsda`);
+        
+//     }
+let elInp=document. getElementById("inp")
+let elBtn=document.getElementById("btn")
+
+
+if( elInp=1){
+    console.log("duyshanba");
+    
+}
+else if(elinp=2){
+    console.log("seyshanba ");
+    
+}
